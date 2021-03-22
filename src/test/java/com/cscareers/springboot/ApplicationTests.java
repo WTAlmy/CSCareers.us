94e2215e8380c93ef3338718d2b6210c549a4e55
William Almy <walmy@ucsc.edu>
2021-03-22 00:52:54 -0700

    Init

---
+++ src/test/java/com/cscareers/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cscareers.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
