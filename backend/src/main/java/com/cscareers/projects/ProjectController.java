fe2910fe692cbc209210e75b279a58b1b2985fba
daniel-carrera <dcarrer1@ucsc.edu>
2021-03-23 23:51:04 -0700

    added cross origin

--- backend/src/main/java/com/cscareers/projects/ProjectController.java
+++ backend/src/main/java/com/cscareers/projects/ProjectController.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin("*")
 @RestController
 public class ProjectController {
 	
